4ba83bfa7955d54c47362f8283f1c151c10331e1
Rd-Trends <69491160+Rd-Trends@users.noreply.github.com>
2021-02-08 02:44:16 +0100

    Added sw.js right file path

--- src/JS/main.js
+++ src/JS/main.js
@@ -384,7 +384,7 @@ document.addEventListener("click", closeAllSelect);
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('./sw.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
